077c70beebed0dbf5e00338c301cf8b5d8aec719
Dmitrii Sobin <neo7976@yandex.ru>
2022-07-19 11:24:06 +0300

    Задача №2. Написание Worker.java

--- src/task2/Worker.java
+++ src/task2/Worker.java
@@ -3,4 +3,15 @@ package task2;
 public class Worker {
 
     private OnTaskDoneListener callback;
+
+    public Worker(OnTaskDoneListener callback) {
+        this.callback = callback;
+    }
+
+    public void start(){
+        for (int i = 0; i < 100; i++) {
+            callback.onDone("Task " + i + " is done\n");
+        }
+    }
+
 }
