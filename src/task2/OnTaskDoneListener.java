37a1c6aed2afeead895dd3eb874bfc7600526af0
Dmitrii Sobin <neo7976@yandex.ru>
2022-07-19 11:04:58 +0300

    Задача №2. Написание кода

---
+++ src/task2/OnTaskDoneListener.java
@@ -0,0 +1,6 @@
+package task2;
+
+@FunctionalInterface
+public interface OnTaskDoneListener {
+    void onDone(String result);
+}
---
+++ src/task2/Worker.java
@@ -0,0 +1,6 @@
+package task2;
+
+public class Worker {
+
+    private OnTaskDoneListener callback;
+}
