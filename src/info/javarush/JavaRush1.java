4384bd713461ea5c50bb8bd0d0a26b835ba2a383
Dmitrii Sobin <neo7976@yandex.ru>
2022-07-20 11:52:41 +0300

    JavaRush1.java

---
+++ src/info/javarush/JavaRush1.java
@@ -0,0 +1,39 @@
+package info.javarush;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class JavaRush1 {
+
+    public static void main(String [] a)  {
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        System.out.print("Выводит все числа: ");
+        evaluate(list, (n)->true);
+
+        System.out.print("Не выводит ни одного числа: ");
+        evaluate(list, (n)->false);
+
+        System.out.print("Вывод четных чисел: ");
+        evaluate(list, (n)-> n%2 == 0 );
+
+        System.out.print("Вывод нечетных чисел: ");
+        evaluate(list, (n)-> n%2 == 1 );
+
+        System.out.print("Вывод чисел больше 5: ");
+        evaluate(list, (n)-> n > 5 );
+
+    }
+
+    public static void evaluate(List<Integer> list, Predicate<Integer> predicate) {
+        for(Integer n: list)  {
+            if(predicate.test(n)) {
+                System.out.print(n + " ");
+            }
+        }
+        System.out.println();
+    }
+
+}
