c09ae9a9a142b052c127a95fcc37009d89c788a2
Dmitrii Sobin <neo7976@yandex.ru>
2022-07-25 13:43:55 +0300

    JavaRush2.java

--- src/info/javarush/JavaRush2.java
+++ src/info/javarush/JavaRush2.java
@@ -1,6 +1,7 @@
 package info.javarush;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 //Скорее всего информация к следующей лекции по Stream
@@ -15,8 +16,17 @@ public class JavaRush2 {
         System.out.println();
 
         List<Integer> list2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-        list2.stream()
-                .map((x) -> x * x + " ")
-                .forEach(System.out::print);
+        list2.stream().map((x) -> x * x + " ").forEach(System.out::print);
+
+        System.out.println();
+        System.out.println("Возводим и выводим только чётные числа:");
+        list2.stream().filter(x -> x % 2 == 0).map((x) -> x * x + " ").forEach(System.out::print);
+
+        System.out.println();
+        System.out.println("Возводим только чётные числа, пробуем вывести все:");
+        list2.stream().map((x) -> {
+            if (x % 2 == 0) return x * x + " ";
+            else return x + " ";
+        }).forEach(System.out::print);
     }
 }
